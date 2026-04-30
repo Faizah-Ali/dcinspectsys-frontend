5ae05e09caf022c7ddfc15b5a96a32e2c5171b8c
Faizah Ali <faizahali2002@gmail.com>
2026-04-30 12:24:16 +0530

    fix/image-path

--- src/common/constants/images.ts
+++ src/common/constants/images.ts
@@ -2,9 +2,9 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import PrintIcon from '@mui/icons-material/Print';
 
-const DhcLogo = "/assets/images/dhclogo.png";
-const Emblem = "/assets/images/emblem.png";
-const CourtBuilding = "/assets/images/dhc3.jpg";
+import DhcLogo from "../../assets/images/dhclogo.png";
+import Emblem from "../../assets/images/emblem.png";
+import CourtBuilding from "../../assets/images/dhc3.jpg";
 
 export const IMAGES = {
     DhcLogo,
