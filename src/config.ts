bdf37e896d4128f7774ad63a1fdf8015aca26a81
Faizah Ali <faizahali2002@gmail.com>
2026-05-11 15:14:22 +0530

    fix/base-url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:8080"
-    : "http://172.16.114.198:8080/inspection-system";
+export const BASE_URL = "/inspection-system";
